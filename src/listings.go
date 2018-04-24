99d9be0b403b285ed615aa0d46eb86a06259d88e
Patrick <patrick.mitchell95@gmail.com>
2018-04-24 16:14:13 +1000

    Handle getting all listings

--- src/listings.go
+++ src/listings.go
@@ -30,6 +30,10 @@ type Listing struct {
 	TotalArea     float32 `json:"total_area"`
 }
 
+type Listings struct {
+	Listings []*Listing
+}
+
 // execute the HTTP requests and get the single Listing that should come out
 func execRequestReturnSingleListing(s *Session, req *http.Request) (*Listing, error) {
 	responseBytes, err := executeRequestAndGetBodyBytes(s, req)
@@ -37,19 +41,61 @@ func execRequestReturnSingleListing(s *Session, req *http.Request) (*Listing, er
 		return nil, err
 	}
 
-	var c map[string]*Listing
+	var temp map[string]*Listing
 
-	err = json.Unmarshal(responseBytes, &c)
+	err = json.Unmarshal(responseBytes, &temp)
 	if err != nil {
 		return nil, err
 	}
 
 	var v *Listing
-	for k, v := range c {
+	for k, v := range temp {
 		v.ID = k
 	}
 
 	return v, nil
+}
+
+//WIP
+func execRequestReturnListings(s *Session, req *http.Request) (*Listings, error) {
+	responseBytes, err := executeRequestAndGetBodyBytes(s, req)
+	if err != nil {
+		return nil, err
+	}
+
+	//What the response should look like.
+	// {[
+	// 		ID [fields:{data:val, data:val}]
+	// 		ID [fields:{data:val, data:val}]
+	//	 	ID [fields:{data:val, data:val}]
+	// ]}
+
+	//	first unmarshal the raw json into a valid interface
+	//	then iterate through one result at a time
+	//	the structure should be the same as a listing, just an array
+	var temp []map[string]*Listing
+
+	//	unmarshal each listing into a listing struct,
+	err = json.Unmarshal(responseBytes, &temp)
+	if err != nil {
+		return nil, err
+	}
+
+	var list *Listings
+	var listing *Listing
+	//	squeeze the leading ID into the listing struct
+	// 	then append them to a list and push them into the listings struct
+	for index, entry := range temp {
+		for id, listing := range entry {
+			listing.ID = id
+			list.Listings = append(list.Listings, listing)
+		}
+	}
+
+	//need to unpack the leading string into the listing ID
+
+	return list, nil
+
 }
 
 // GetListingByID gets a single listing by its ID
@@ -89,6 +135,24 @@ func (s *Session) GetListingByName(name string) (*Listing, error) {
 	}
 
 	return execRequestReturnSingleListing(s, req)
+}
+
+//GetListings returns a slice of all Listing
+func (s *Session) GetListings() ([]*Listing, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v",
+			s.Auth.PortalEndpoint,
+			listingEndpoint),
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnListings(s, req)
 }
 
 // SendUpdate saves changes to listing
