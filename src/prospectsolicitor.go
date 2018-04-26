f61dabfc6ee39b07ea01d3114137b8c2acd83d0d
Patrick <patrick.mitchell95@gmail.com>
2018-04-26 17:41:23 +1000

    Solicitor framework included

--- src/prospectsolicitor.go
+++ src/prospectsolicitor.go
@@ -1,4 +1,34 @@
 package portal
 
 type ProspectSolicitor struct {
+	ID          string
+	Phone       string
+	Mobile      string
+	Email       string
+	CompanyName string
+	FullName    string
+	AddressID   string
+	Address     Address
+	CompanyID   string
+	ProjectID   string
+}
+
+//GetSolicitor GETs a SolicitorID
+func GetSolicitor() {
+
+}
+
+//CreateSolicitor POSTs new solicitor information to a Prospect
+func CreateSolicitor() {
+
+}
+
+//UpdateSolicitor PUTs new information to an existing solicitor
+func UpdateSolicitor() {
+
+}
+
+//DeleteSolicitor DELETEs a solicitor from a prospect
+func DeleteSolicitor() {
+
 }
