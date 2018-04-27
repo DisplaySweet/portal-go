8523bdc92fd0c4ec912911549f2d2d78041bda58
Patrick <patrick.mitchell95@gmail.com>
2018-04-27 10:26:21 +1000

    Offer (except attachment-related methods) ready for testing & correction

--- src/offer.go
+++ src/offer.go
@@ -1,5 +1,14 @@
 package portal
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const offerEndpoint = "offers"
+
 type Offer struct {
 	ID                     string
 	CompanyID              string
@@ -20,51 +29,221 @@ type Offer struct {
 	OfferStatusChangedDate string
 }
 
-//GetAllOffers returns a list of Offers
-func GetAllOffers() {
+func execRequestReturnAllOffers(s *Session, req *http.Request) ([]*Offer, error) {
+	responseBytes, err := executeRequestAndGetBodyBytes(s, req)
+	if err != nil {
+		return nil, err
+	}
 
+	var temp map[string]*Offer
+
+	list := make([]*Offer, 0, 0)
+
+	err = json.Unmarshal(responseBytes, &temp)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, offer := range temp {
+		list = append(list, offer)
+	}
+
+	return list, nil
+}
+
+func execRequestReturnSingleOffer(s *Session, req *http.Request) (*Offer, error) {
+	responseBytes, err := executeRequestAndGetBodyBytes(s, req)
+	if err != nil {
+		return nil, err
+	}
+
+	offer := &Offer{}
+	err = json.Unmarshal(responseBytes, offer)
+
+	return offer, err
+}
+
+//GetAllOffers returns a list of Offers -- this won't work yet
+func (s *Session) GetAllOffers() ([]*Offer, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v",
+			s.Auth.PortalEndpoint,
+			offerEndpoint),
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnAllOffers(s, req)
 }
 
 //GetPendingOffers returns a list of offers where status is pending
-func GetPendingOffers() {
+func (s *Session) GetPendingOffers() ([]*Offer, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v/pending",
+			s.Auth.PortalEndpoint,
+			offerEndpoint),
+		nil,
+	)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnAllOffers(s, req)
 }
 
 //GetCompletedOffers returns a list of offers where status is complete
-func GetCompletedOffers() {
+func (s *Session) GetCompletedOffers() ([]*Offer, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v/completed",
+			s.Auth.PortalEndpoint,
+			offerEndpoint),
+		nil,
+	)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnAllOffers(s, req)
 }
 
 //GetOfferByID returns a single offer given the offer ID
-func GetOfferByID() {
+func (s *Session) GetOfferByID(id string) (*Offer, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v/%v",
+			s.Auth.PortalEndpoint,
+			offerEndpoint,
+			id),
+		nil,
+	)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnSingleOffer(s, req)
 }
 
 //CreateOffer POSTs a new offer and reserves the relevant listing
-func CreateOffer() {
+func (o *Offer) CreateOffer(s *Session) (*Offer, error) {
+	o.ID = ""
+	body, err := json.Marshal(*o)
+	if err != nil {
+		return nil, err
+	}
 
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf(
+			"%v/%v",
+			s.Auth.PortalEndpoint,
+			offerEndpoint,
+		),
+		bytes.NewReader(body),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnSingleOffer(s, req)
 }
 
 //UpdateOffer PUTs new data to an existing offer, using an offer ID
-func UpdateOffer() {
+func (o *Offer) UpdateOffer(s *Session) (*Offer, error) {
+	body, err := json.Marshal(*o)
+	if err != nil {
+		return nil, err
+	}
 
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf(
+			"%v/%v/%v",
+			s.Auth.PortalEndpoint,
+			offerEndpoint,
+			o.ID),
+		bytes.NewReader(body),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnSingleOffer(s, req)
 }
 
 //CompleteOffer PUTs new data to an existing offer, changing the offer status to complete and the relevant listing to sold.
-func CompleteOffer() {
+func (o *Offer) CompleteOffer(s *Session) (*Offer, error) {
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf(
+			"%v/%v/%v/compelete",
+			s.Auth.PortalEndpoint,
+			offerEndpoint,
+			o.ID),
+		nil,
+	)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnSingleOffer(s, req)
 }
 
+//TODO: Unsure how this is meant to work
 //AttachDoc POSTs an attachment to the offer
-func AttachDoc() {
+func (o *Offer) AttachDoc(s *Session) (*Offer, error) {
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf(
+			"%v/%v/%v/attachments",
+			s.Auth.PortalEndpoint,
+			offerEndpoint,
+			o.ID),
+		nil,
+	)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnSingleAttachment(s, req)
 }
 
+//TODO: Unsure how this is meant to work
 //DownloadOfferAttachment downloads a specific attachment on an offer give the attachment & offer id
 func DownloadOfferAttachment() {
 }
 
 //CancelOffer sets an offer's status to cancelled and reverts a listing to available
-func CancelOffer() {
+func (o *Offer) CancelOffer(s *Session) (*Offer, error) {
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf(
+			"%v/%v/%v/cancel",
+			s.Auth.PortalEndpoint,
+			offerEndpoint,
+			o.ID),
+		nil,
+	)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return execRequestReturnSingleOffer(s, req)
 }
