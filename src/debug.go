17fdbaa74d07aba060e16b035d6e2428d5d9b6d9
Patrick <patrick.mitchell95@gmail.com>
2018-05-04 09:09:02 +1000

    Caller(0) runtime info of the method calling it. should be caller(1)

--- src/debug.go
+++ src/debug.go
@@ -3,11 +3,11 @@ package portal
 import "runtime"
 
 func ErrorFile() string {
-	_, file, _, _ := runtime.Caller(0)
+	_, file, _, _ := runtime.Caller(1)
 	return file
 }
 
 func ErrorLine() int {
-	_, _, line, _ := runtime.Caller(0)
+	_, _, line, _ := runtime.Caller(1)
 	return line
 }
