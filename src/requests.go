2bc0e946cf0e1b32ad56ed35976bc1b119270c81
Patrick <patrick.mitchell95@gmail.com>
2018-05-07 12:11:33 +1000

    reflect naming changes

--- src/requests.go
+++ src/requests.go
@@ -13,8 +13,8 @@ func executeRequest(s *Session, req *http.Request) (*http.Response, error) {
 	req.Header["X-DS-AUTH-APITOKEN"] = []string{s.Auth.APIKey}
 	//req.Header["X-DS-AUTH-COMPANY"] = []string{s.Auth.Company}
 	req.Header["X-DS-DATA-APITOKEN"] = []string{s.Auth.APIKey}
-	req.Header["X-DS-DATA-COMPANY"] = []string{s.Company}
-	req.Header["X-DS-DATA-PROJECT"] = []string{s.Project}
+	req.Header["X-DS-DATA-COMPANY"] = []string{s.CompanyID}
+	req.Header["X-DS-DATA-PROJECT"] = []string{s.ProjectID}
 
 	return http.DefaultClient.Do(req)
 }
