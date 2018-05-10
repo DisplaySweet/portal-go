19852836034b478f0e06721fc4da663f0d754fd1
Patrick <patrick.mitchell95@gmail.com>
2018-05-10 15:42:48 +1000

    finalise users functionality implementation

--- src/users.go
+++ src/users.go
@@ -39,7 +39,21 @@ func execRequestReturnSingleUser(s *Session, req *http.Request) (*User, error) {
 	return user, err
 }
 
-///////GetUserCompany
+func execRequestReturnMultipleUsers(s *Session, req *http.Request) ([]*User, error) {
+	responseBytes, err := executeRequestAndGetBodyBytes(s, req)
+	if err != nil {
+		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
+		return nil, err
+	}
+
+	var response []*User
+	err = json.Unmarshal(responseBytes, response)
+	for _, user := range response {
+		user.S = *s
+	}
+
+	return response, err
+}
 
 //GetUsers gets all users
 func (s *Session) GetUsers() ([]*User, error) {
@@ -58,24 +72,65 @@ func (s *Session) GetUsers() ([]*User, error) {
 		return nil, err
 	}
 
-	responseBytes, err := executeRequestAndGetBodyBytes(s, req)
+	return execRequestReturnMultipleUsers(s, req)
+}
+
+func (u *User) GetAccountsContacts() ([]*Account, []*Contact, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v/%v/accountscontacts",
+			u.S.Auth.PortalEndpoint,
+			userEndpoint,
+			u.ID,
+		),
+		nil,
+	)
+	if err != nil {
+		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
+		return nil, nil, err
+	}
+
+	return execRequestReturnAllAccountsContacts(&u.S, req)
+}
+
+func (u *User) GetCompanyUsers() ([]*User, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v/mycompany",
+			u.S.Auth.PortalEndpoint,
+			userEndpoint,
+		),
+		nil,
+	)
 	if err != nil {
 		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
 		return nil, err
 	}
 
-	var response []*User
-	err = json.Unmarshal(responseBytes, response)
-	for _, user := range response {
-		user.S = *s
-	}
-
-	return response, err
+	return execRequestReturnMultipleUsers(&u.S, req)
 }
 
-///////MycompanyGetUsers
-
 //////EOIUSers
+func (u *User) GetEOIUsers() ([]*User, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"%v/%v/eoi-user",
+			u.S.Auth.PortalEndpoint,
+			userEndpoint,
+		),
+		nil,
+	)
+	if err != nil {
+		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
+		return nil, err
+	}
+
+	return execRequestReturnMultipleUsers(&u.S, req)
+
+}
 
 // GetUserByID gets a single contact by its ID
 func (s *Session) GetUserByID(id string) (*User, error) {
@@ -163,6 +218,33 @@ func (u *User) SendUpdate() error {
 	return nil
 }
 
-///////DELETE
+func (u *User) Delete() error {
+	body, err := json.Marshal(*u)
+	if err != nil {
+		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
+		return err
+	}
 
-//GET ACCOUNTSCONTACTS
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf(
+			"%v/%v/%v",
+			u.S.Auth.PortalEndpoint,
+			userEndpoint,
+			u.ID,
+		),
+		bytes.NewReader(body),
+	)
+
+	if err != nil {
+		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
+		return err
+	}
+
+	err = executeRequestAndParseStatusCode(&u.S, req)
+	if err != nil {
+		err = fmt.Errorf("Error in file: %v line %v. Original ERR: %v", ErrorFile(), ErrorLine(), err)
+		return err
+	}
+	return nil
+}
